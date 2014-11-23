436241b6463fdf4014b6bf7c8016f239f951ef89
MaHuJa <github@mahuja.net>
2014-11-23 11:55:11 +0100

    Placeline no longer limited to a single stack

--- robot/placeline.lua
+++ robot/placeline.lua
@@ -3,8 +3,22 @@ robot = require "robot"
 length = tonumber(...)
 if not length then error "Length needed" end
 
+function place(fn)
+  if fn() then return true end
+  if robot.detectDown() then return true end
+  if robot.count(robot.select()) == 0 then
+    for i = 1,16 do
+      if robot.count(i) > 0 then
+        robot.select(i);
+        return fn();
+      end
+    end
+  end
+  return false, "Unknown error in place()";
+end
+
 for _ = 1,length do
-  robot.placeDown()
-  robot.forward()
+  assert(place(robot.placeDown))
+  assert(robot.forward())
   io.write '.'
 end
