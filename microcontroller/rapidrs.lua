b391623d02835fed0c19e2d5abf60b1b041caca6
MaHuJa <github@mahuja.net>
2015-05-05 13:05:42 +0200

    Rapidrs now with sleeps

--- microcontroller/rapidrs.lua
+++ microcontroller/rapidrs.lua
@@ -1,9 +1,11 @@
 -- Toggle RS rapidly
 local side = 5 -- left
-
+wait = computer.pullSignal;
 rs = component.proxy(component.list("redstone")())
 while true do
   rs.setOutput(side,255);
+  wait(0.05);
   rs.setOutput(side,0);
+  wait(0.05);
 end
 
