1cf455ab4f21fb3ecfd2d866496f9c986b12c805
MaHuJa <github@mahuja.net>
2015-05-25 01:40:56 +0200

    Rapidrs speed fix
    I think the wait(0.05) had it span each state across two ticks.

--- microcontroller/rapidrs.lua
+++ microcontroller/rapidrs.lua
@@ -4,8 +4,8 @@ wait = computer.pullSignal;
 rs = component.proxy(component.list("redstone")())
 while true do
   rs.setOutput(side,255);
-  wait(0.05);
+  wait(0.03);
   rs.setOutput(side,0);
-  wait(0.05);
+  wait(0.03);
 end
 
