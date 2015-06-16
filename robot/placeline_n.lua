53968063a5deb2d3bd2ec20eee2c79d43d288785
MaHuJa <github@mahuja.net>
2015-06-16 22:16:50 +0200

    Robot placeline to end all variants

---
+++ robot/placeline_n.lua
@@ -0,0 +1,48 @@
+robot = require "robot"
+
+params = {...};
+if params[1] == '-h' then
+  print (
+[[Usage: placeline_n stepsize countsteps function
+  stepsize - place this many blocks (3)");
+  countsteps - how many columns to make (infinite");
+  function - what to do after each step ("robot.down()")
+]]);
+  return false;
+end
+
+assert (not params[1] or tonumber(params[1]), "Stepsize param is not a number");
+local stepsize = tonumber(params[1]) or 3;
+
+assert (not params[2] or tonumber[(params[2]), "Count param is not a number");
+local length = tonumber(params[2]) or math.huge;
+
+local func = params[3] and assert(load(params[3])) or robot.down();
+
+function place()
+  if robot.placeDown() or robot.detectDown() then return true end
+  if robot.count(robot.select()) == 0 then
+    for i = 1,16 do
+      if robot.count(i) > 0 then
+        robot.select(i);
+        return fn();
+      end
+    end
+  end
+  return false, "Cannot place(). Out of blocks?";
+end
+
+local function forward()
+	assert(place())
+	assert(robot.forward())
+end
+
+
+for _ = 1,length do
+  forward()
+  forward()
+  forward()
+  assert(func())
+  io.write '.'
+end
+
