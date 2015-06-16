2bff1a3543cab8f6e3c2cbaef0103a572a694f0a
MaHuJa <github@mahuja.net>
2015-06-16 12:59:30 +0200

    Place descending line

---
+++ robot/placeline_descending.lua
@@ -0,0 +1,34 @@
+robot = require "robot"
+
+params = {...};
+local stepsize = tonumber(params[1]) or 3;
+length = tonumber(params[2]) or math.huge;
+
+function place(fn)
+  if fn() then return true end
+  if robot.detectDown() then return true end
+  if robot.count(robot.select()) == 0 then
+    for i = 1,16 do
+      if robot.count(i) > 0 then
+        robot.select(i);
+        return fn();
+      end
+    end
+  end
+  return false, "Cannot place(). Out of blocks?";
+end
+
+local function forward()
+	assert(place(robot.placeDown))
+	assert(robot.forward())
+end
+
+
+for _ = 1,length do
+  forward()
+  forward()
+  forward()
+  assert(robot.down())
+  io.write '.'
+end
+
